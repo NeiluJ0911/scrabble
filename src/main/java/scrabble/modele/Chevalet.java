d18187d3fbec68f5e5d8c05c2448a4d73596786c
Julien Coulaud <julien.coulaud@etu.unilim.fr>
2024-04-29 11:34:28 +0200

    ajout Chevalet

---
+++ src/main/java/scrabble/modele/Chevalet.java
@@ -0,0 +1,35 @@
+package scrabble.modele;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import scrabble.gui.Console;
+
+public class Chevalet {
+	List<Jeton> jetonsJouables; 
+	
+	Chevalet(){
+		this.jetonsJouables = new ArrayList<Jeton>();
+	}
+	
+	void ajouter(Jeton jeton) {
+		this.jetonsJouables.add(jeton);
+	}
+	
+	void jouer(Jeton jeton){
+		// TODO Ajouter l'envoi d'un jeton vers une case du plateau
+	}
+	
+	void afficher() {
+	    if (jetonsJouables.isEmpty()) {
+	        Console.message("Le chevalet est vide."); // Affiche un message si le chevalet est vide
+	    } else {
+	        Console.message("Jetons sur le chevalet :");
+	        for (Jeton jeton : jetonsJouables) {
+	            Console.message("Lettre : " + jeton.getLettre() + ", Points : " + jeton.getPoints().getValeur());
+	        }
+	    }
+	}
+
+	
+}
