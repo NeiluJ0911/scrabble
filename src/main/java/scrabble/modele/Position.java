9007b28ba6bcbff7e3d3a3c5dc9e0a7593fdb05e
Mateo Deterne <mateodeterne@gmail.com>
2024-05-02 15:52:53 +0200

    ajout class position

---
+++ src/main/java/scrabble/modele/Position.java
@@ -0,0 +1,28 @@
+package scrabble.modele;
+
+public class Position {
+	
+	private Integer colonne;
+	private Integer ligne;
+
+	public Position(Integer ligne, Integer colonne) {
+		this.colonne = colonne;
+		this.ligne = ligne;
+	}
+
+	public Integer getColonne() {
+		return colonne;
+	}
+
+	public void setColonne(Integer colonne) {
+		this.colonne = colonne;
+	}
+
+	public Integer getLigne() {
+		return ligne;
+	}
+
+	public void setLigne(Integer ligne) {
+		this.ligne = ligne;
+	}
+}
